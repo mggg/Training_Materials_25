9d514e08746e9a7b9c561935be12c33b192ad82d
peterrrock2 <peter.r.rock2@gmail.com>
2025-06-17 21:43:11 -0600

    Update Thursday batching stuff

--- notebooks/technical/Tech_3_batching/districting/gerrychain_run_single.py
+++ notebooks/technical/Tech_3_batching/districting/gerrychain_run_single.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def run_single_chain():
     graph_file = Path("../../../../data/gerrymandria.json").resolve()
 
-    output_dir = Path("../chain_out").resolve()
+    output_dir = Path("./chain_out").resolve()
     output_dir.mkdir(parents=True, exist_ok=True)
 
     run_chain(
